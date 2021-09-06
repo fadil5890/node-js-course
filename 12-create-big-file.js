73a58fc18df35e9177b74510ce49befba3888642
Fadil <fadilakbar7770@gmail.com>
2021-09-06 08:57:40 +0700

    learn difference performance between sync & async

---
+++ 12-create-big-file.js
@@ -0,0 +1,13 @@
+const { writeFile } = require('fs').promises;
+console.time()
+const start = async () => {
+    try {
+        for(let i = 0; i <= 10000; i++){
+            await writeFile('./module-intro/big-file-async.txt', `HELLO WORLD ${i} \n`, {flag :'a'});
+        }
+    } catch (error) {
+        console.log(error);       
+    }
+}
+start();
+console.timeEnd()
---
+++ 13-create-big-file-sync.js
@@ -0,0 +1,6 @@
+const { writeFileSync } = require('fs');
+console.time()
+for(let i = 0; i <= 10000; i++){
+    writeFileSync('./module-intro/big-file.txt', `HELLO WORLD ${i} \n`, {flag : 'a'});
+}
+console.timeEnd()
