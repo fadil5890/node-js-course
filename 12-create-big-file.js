43f7d1c6d00be4cba34724b6109bc58850a210b5
Fadil <fadilakbar7770@gmail.com>
2021-09-06 19:16:44 +0700

    learn createReadStream

--- 12-create-big-file.js
+++ 12-create-big-file.js
@@ -2,7 +2,7 @@ const { writeFile } = require('fs').promises;
 console.time()
 const start = async () => {
     try {
-        for(let i = 0; i <= 10000; i++){
+        for(let i = 0; i <= 100000; i++){
             await writeFile('./module-intro/big-file-async.txt', `HELLO WORLD ${i} \n`, {flag :'a'});
         }
     } catch (error) {
--- 13-create-big-file-sync.js
+++ 13-create-big-file-sync.js
@@ -1,6 +1,6 @@
 const { writeFileSync } = require('fs');
 console.time()
-for(let i = 0; i <= 10000; i++){
+for(let i = 0; i <= 100000; i++){
     writeFileSync('./module-intro/big-file.txt', `HELLO WORLD ${i} \n`, {flag : 'a'});
 }
 console.timeEnd()
